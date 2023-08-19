fd1a57a59861845c243750c64752d88f3f4247ef
Adam Socki <socki.adam@gmail.com>
2023-08-19 11:09:20 -0500

    place/grid integration

--- scripts/classes/grid.gd
+++ scripts/classes/grid.gd
@@ -4,39 +4,30 @@ class_name Grid
 var grid_size : Vector2
 var places = []
 
-var occupied_locations = []
 
  
 func is_location_occupied(location_to_check: Vector2) -> bool:
-	if occupied_locations == null:
+	if places == null:
 		return false
 	else:
-		for location in occupied_locations:
-			if location == location_to_check:
+		for place in places:
+			if place.grid_location == location_to_check:
 				return true
 	return false
 	
 
-func generate_new_place() -> Place:
+func generate_new_place():
 	var create_place = true
 	var location : Vector2
 	while create_place:
 		location.x = randi_range(0, grid_size.x)
 		location.y = randi_range(0, grid_size.y)
-		create_place = is_location_occupied(location)
-	occupied_locations.append(location)
 	var generated_place = Place.new(location)
-	return generated_place
+	places.append(generated_place)
 
 func init_places():
-	print("2")
-	print(Global.places_to_init)
 	for i in Global.places_to_init:
-		print("1")
-		
-		places.append(generate_new_place())
-		print(places[i].grid_location)
-
+		generate_new_place()
 
 
 func _init():
